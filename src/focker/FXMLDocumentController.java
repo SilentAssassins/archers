237f52572b8f86926007d26c3c60fd611ab143aa
Menake <Menake@172.20.10.3>
2017-06-21 00:17:36 +0530

    Little array changes

--- src/focker/FXMLDocumentController.java
+++ src/focker/FXMLDocumentController.java
@@ -89,7 +89,7 @@ public class FXMLDocumentController implements Initializable {
         private int faceValue;
         
         public boolean isFirstRoundCompleted = false;
-        public String temp[] = {"02.png","03.png","04.png","05.png","06.png","07.png","08.png","09.png","10.png","11.png","12.png","14.png","15.png","16.png","17.png","18.png","19.png","20.png","21.png","22.png","23.png","24.png","25.png","26.png","27.png","28.png","29.png","30.png","31.png","32.png","33.png","34.png","35.png","36.png","37.png","38.png","39.png","40.png","41.png","42.png","43.png","44.png","45.png","46.png","47.png","48.png","49.png","50.png","51.png","52.png","53.png"};
+        public String temp[] = {"2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png","12.png","14.png","15.png","16.png","17.png","18.png","19.png","20.png","21.png","22.png","23.png","24.png","25.png","26.png","27.png","28.png","29.png","30.png","31.png","32.png","33.png","34.png","35.png","36.png","37.png","38.png","39.png","40.png","41.png","42.png","43.png","44.png","45.png","46.png","47.png","48.png","49.png","50.png","51.png","52.png","53.png"};
         //public static final String[]deal1 = new String[40];
         
         public Cards1() throws FileNotFoundException 
@@ -97,13 +97,13 @@ public class FXMLDocumentController implements Initializable {
             //ImageA = new ImageView(new Image(getClass().getResourceAsStream("/Resources/Images/cards/02.png")));
             //ImageA.setImage(new Image(getClass().getResourceAsStream("res/Images/cards/02.png")));
             
-            TranslateTransition transition = new TranslateTransition(); 
-            transition.setDuration(Duration.millis(50));
-            transition.setNode(ImageA);
-
-            transition.setToY(-200);
-
-            transition.play();
+//            TranslateTransition transition = new TranslateTransition(); 
+//            transition.setDuration(Duration.millis(50));
+//            transition.setNode(ImageA);
+//
+//            transition.setToY(-200);
+//
+//            transition.play();
            //CardSelect();
         }
         
@@ -117,7 +117,15 @@ public class FXMLDocumentController implements Initializable {
          */
         public void CardSelect() throws IOException
         {
-            String cards[] = {"02.png","03.png","04.png","05.png","06.png","07.png","08.png","09.png","10.png","11.png","12.png","14.png","15.png","16.png","17.png","18.png","19.png","20.png","21.png","22.png","23.png","24.png","25.png","26.png","27.png","28.png","29.png","30.png","31.png","32.png","33.png","34.png","35.png","36.png","37.png","38.png","39.png","40.png","41.png","42.png","43.png","44.png","45.png","46.png","47.png","48.png","49.png","50.png","51.png","52.png","53.png"};
+            int cd=2;
+            String cards[] = new String[52];//{"02.png","03.png","04.png","05.png","06.png","07.png","08.png","09.png","10.png","11.png","12.png","14.png","15.png","16.png","17.png","18.png","19.png","20.png","21.png","22.png","23.png","24.png","25.png","26.png","27.png","28.png","29.png","30.png","31.png","32.png","33.png","34.png","35.png","36.png","37.png","38.png","39.png","40.png","41.png","42.png","43.png","44.png","45.png","46.png","47.png","48.png","49.png","50.png","51.png","52.png","53.png"};
+            for(int m = 0;m<52;m++)
+            {
+                
+                cards[m] = cd+".png";
+                cd++;
+                //System.err.println(cards[m]);
+            }
             String player1[] = new String[2];
             String player2[] = new String[2];
             String player3[] = new String[2];
@@ -263,7 +271,7 @@ public class FXMLDocumentController implements Initializable {
                     transition10.setNode(ImageE1);
 
                     transition10.setToY(-50);
-                    transition10.setToX(275);
+                    transition10.setToX(325);
 
                     transition10.play();
                     break;
